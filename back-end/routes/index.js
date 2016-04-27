e631bf60980c14843e4193b9bfae136bd11f46ba
curtjenk <curtjenk@comcast.net>
2016-04-27 13:36:12 -0400

    More stuff while development

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
+router.post('/search', function(req, res, next) {
+  var name = req.body.name;
+  var dcClass = ['Tristan', 'Josh', 'Keith', 'Bogdan', 'Will', 'Curtis',
+     'Joe', 'Kochan', 'Patrick', 'Jonathan', 'Jeremy'];
+
+  if (dcClass.indexOf(name) > -1) {
+       res.json({message: "Hello, " + name + " you are a student"});
+  } else {
+       res.json({message: "You are not in this class"});  //respond with a json object
+}
+
+})
 module.exports = router;
--- front-end/controller.js
+++ front-end/controller.js
@@ -1,6 +1,17 @@
 var searchApp = angular.module("searchApp", []);
 searchApp.controller("searchController", function($scope, $http) {
-  $scope.search = function() {
-    $scope.message = "hello world";
-  }
+    $scope.search = function() {
+        $scope.message = "hello world";
+
+        var apiUrl = 'http://localhost:3000/search';
+
+        $http.post(apiUrl, { "name": $scope.who }).then(
+            function(response) {
+                $scope.result = response.data.message;
+            },
+            function(error) {
+                $scope.result = "Error!";
+            }
+        )
+    }
 })
